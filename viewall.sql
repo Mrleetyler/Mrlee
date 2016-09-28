98f877b1d9ec839dc4e02f01c5bed22ea1d3837b
Mrleetyler <Lee.tyler2@virginaustralia.com>
2016-09-28 16:18:18 +1000

    Add files via upload

---
+++ viewall.sql
@@ -0,0 +1,20 @@
+select * from wo
+where ac = 'VH-YIZ';
+select * from ac_master
+where ac = 'VH-YIZ';
+select * from engineering_order;
+select * from defect_report;
+select * from ac_actual_flights
+where ac = 'VH-YIZ';
+select * from task_card_item;
+select * from wo_task_card_item;
+select * from engineering_effectivity;
+select * from engineering_schedule_control;
+select * from engineering_control;
+select * from LOCATION_MASTER;
+select * from wo_task_card;
+select * from task_Card;
+select * from defect_report
+
+
+
